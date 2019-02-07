1cdd66fe0be95f6553fd3b34e0e6fdb421ac66cc
Kat Maldon <katharinamaldon@gmail.com>
2019-02-07 18:38:10 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end
 
   def parse
-    email_array = @email.split(/[" "]/).uniq
+    email_array = @email.split(/[, ]/).uniq
     email_array.reject! {|element| element.empty?}
     email_array
   end
